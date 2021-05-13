e4b1c46bb7b60f5b45130149f0aa54aab94b9259
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-05-13 15:57:35 -0400

    Add testing

---
+++ tests/test_server.py
@@ -0,0 +1,42 @@
+"""Test server."""
+import json
+from pathlib import Path
+
+from fastapi import testclient
+from fastapi.testclient import TestClient
+
+from app.server import APP
+
+testclient = TestClient(APP)
+
+THIS_DIR = Path(__file__).parent
+EXAMPLE_DIR = THIS_DIR.parent / "app" / "openapi_examples"
+
+with open(EXAMPLE_DIR / "query.json") as stream:
+    REQUEST = json.load(stream)
+with open(EXAMPLE_DIR / "response.json") as stream:
+    RESPONSE = json.load(stream)
+
+
+def test_query():
+    """Test calling /query endpoint."""
+    response = testclient.post(
+        "/query",
+        json=REQUEST,
+        params={"max_results": 1},
+    )
+    response.raise_for_status()
+    assert response.json() == RESPONSE
+
+
+def test_endpoint():
+    """Test getting OpenAPI."""
+    response = testclient.get(
+        "/openapi.json",
+    )
+    response.raise_for_status()
+    # and again, to make sure the caching works
+    response = testclient.get(
+        "/openapi.json",
+    )
+    response.raise_for_status()
